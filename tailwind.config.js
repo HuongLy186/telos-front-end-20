83db79500044dc3f3cce42c6950abedccc1290b1
huongly <lyhuong.ph99@gmail.com>
2024-11-22 20:48:43 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./*/*.html",
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#e0cec7",
+        secondary: "#977669",
+        abc: "#F4F5F7",
+      },
+      fontSize: {
+        h2: ["48px", "64px"],
+        lead1: ["18px", "32px"],
+        lead2: ["18px", "32px"],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
